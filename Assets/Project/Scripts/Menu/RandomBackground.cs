893602926eece24eaad5bccf16f334785cda263e
JuanK120 <juan.camilo.rosero@correounivalle.edu.co>
2022-02-20 23:36:34 -0500

    creacion de la version inicial  del menu principal del juego

---
+++ Assets/Project/Scripts/Menu/RandomBackground.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ using UnityEngine.UI;
+ 
+ public class RandomBackground : MonoBehaviour {
+ 
+     public Sprite[] Backgrounds; //this is an array of type sprite
+ 
+     public Image img; //this is a variable of type sprite renderer
+ 
+     // Use this for initialization
+     void Start () {
+            img = GetComponent<Image>(); 
+            /*assigning the Render to the object's SpriteRender, this will allow us to access the image from 
+            code*/
+            img.sprite = Backgrounds[Random.Range(0, Backgrounds.Length)]; 
+            /*this will change the current sprite of the sprite renderer to a random sprite that was chosen 
+            randomly from the array of backgrounds */
+         }
+     // Update is called once per frame
+     void Update () {
+ 
+     }
+ }

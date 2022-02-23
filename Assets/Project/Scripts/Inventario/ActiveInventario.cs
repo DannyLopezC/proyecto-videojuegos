9a8dd7ca0b21207201c3630859cbde2137f64146
Johan_David <jhoanpor78@gmail.com>
2022-02-22 21:33:02 -0500

    Inventario V1.2

---
+++ Assets/Project/Scripts/Inventario/ActiveInventario.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveInventario : MonoBehaviour
+{
+    public GameObject Inventario;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetKeyDown(KeyCode.Q))
+        {
+            closeInventario();
+        }
+    }
+
+    public void closeInventario()
+    {
+        Inventario.SetActive(!Inventario.activeInHierarchy);
+    }
+}
